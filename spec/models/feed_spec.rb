f6de701e3ba58c5b148d8f63219ff02c3365d8e2
Simon Walker <s.r.walker101@googlemail.com>
2013-11-09 15:59:02 +0000

    Adds title presence check

--- spec/models/feed_spec.rb
+++ spec/models/feed_spec.rb
@@ -1,17 +1,25 @@
 require 'spec_helper'
 
 describe Feed do
-  it "should have a url" do
-    expect(Feed.new).to have_attribute(:url)
-  end
-
-  context "validations" do
-    it "should fail with an invalid url" do
-      expect(Feed.new(url: "bad url").save).to be_false
+  context "url" do
+    it "should have a url" do
+      expect(Feed.new).to have_attribute(:url)
     end
 
-    it "should fail without a url" do
-      expect(Feed.new.save).to be_false
+    context "validations" do
+      it "should fail with an invalid url" do
+        expect(Feed.new(url: "bad url").save).to be_false
+      end
+
+      it "should fail without a url" do
+        expect(Feed.new.save).to be_false
+      end
+    end
+  end
+
+  context "title" do
+    it "should have a title" do
+      expect(Feed.new).to have_attribute(:title)
     end
   end
 end
