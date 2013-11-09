fac3015ae976c3a745e5aadcb9721d005438743d
Simon Walker <s.r.walker101@googlemail.com>
2013-11-09 15:57:47 +0000

    Adds validations for url

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,2 +1,5 @@
 class Feed < ActiveRecord::Base
+  validates_presence_of :url
+  validates :url, format: { with: /htp:\/\/.+/,
+                            message: "Invalid url format" }
 end
--- spec/models/feed_spec.rb
+++ spec/models/feed_spec.rb
@@ -4,4 +4,14 @@ describe Feed do
   it "should have a url" do
     expect(Feed.new).to have_attribute(:url)
   end
+
+  context "validations" do
+    it "should fail with an invalid url" do
+      expect(Feed.new(url: "bad url").save).to be_false
+    end
+
+    it "should fail without a url" do
+      expect(Feed.new.save).to be_false
+    end
+  end
 end
